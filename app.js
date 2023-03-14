7cf4c09934ce38c73df44740fa06412bf5606cda
Ivan Martinez <51198881+TheOriginalSNP@users.noreply.github.com>
2023-03-14 09:55:04 -0400

    Add  ability to add to todo list

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
+const items = ["Buy Food"];
 app.set("view engine", "ejs");
 
 //app.use(express.static(path.join(__dirname, 'public')));
@@ -16,43 +16,25 @@ app.use(bodyParser.urlencoded({
 app.get("/", function (req, res) {
 
     const today = new Date();
-    const currentDay = today.getDay();
-    let day = "";
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
 
-    switch (currentDay) {
-        case 0:
-            day = "Sunday"
-            break;
-        case 1:
-            day = "Monday"
-            break;
-        case 2:
-            day = "Tuesday"
-            break;
-        case 3:
-            day = "Wednesday"
-            break;
-        case 4:
-            day = "Thursday"
-            break;
-        case 5:
-            day = "Friday"
-            break;
-        case 6:
-            day = "Saturday"
-            break;
-        default:
-            console.log("Error: current day is equal to: " + currentDay);
-    }
+    const day = today.toLocaleDateString("en-US", options);
 
     res.render("list", {
-        kindOfDay: day
+        kindOfDay: day,
+        newListItems: items
     });
 });
 
 app.post("/", function (req, res) {
-
-})
+    const item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
+});
 
 app.listen(3000, function () {
     console.log("Sever 3000 up and running!");
