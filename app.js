bbaf11cd162eafd878df6cb1d767f5661e42c54c
Ivan Martinez <51198881+TheOriginalSNP@users.noreply.github.com>
2023-03-16 10:03:40 -0400

    Separate getDate function into its own ejs

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 const bodyParser = require("body-parser");
 const items = ["Buy Food"];
 const workItems = [];
+const date = require(__dirname + "/date.js");
+
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -14,36 +16,24 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-
 app.get("/", function (req, res) {
-
-    const today = new Date();
-    const options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    const day = today.toLocaleDateString("en-US", options);
-
+    const day = date.getDate()
     res.render("list", {
         listTitle: day,
         newListItems: items
     });
 });
 
-
-app.get("/work", function(req, res) {
+app.get("/work", function (req, res) {
     res.render("list", {
         listTitle: "Work List",
         newListItems: workItems
     });
 });
 
-app.post("/", function(req, res) {
+app.post("/", function (req, res) {
     const item = req.body.newItem;
-
-    if(req.body.list === "Work") {
+    if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
     } else {
@@ -55,4 +45,3 @@ app.post("/", function(req, res) {
 app.listen(3000, function () {
     console.log("Sever 3000 up and running!");
 });
-
