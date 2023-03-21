a7bbb113b72197f3eb8d97c05e2d2d3779756b59
Ivan Martinez <51198881+TheOriginalSNP@users.noreply.github.com>
2023-03-20 20:05:13 -0400

    Add mongodb, mongoose, add new schema

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const path = require('path')
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const items = ["Buy Food"];
-const workItems = [];
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+
+const {
+    MongoClient
+} = require("mongodb");
+const uri = "mongodb+srv://KuraiOG:XyHYvVb5Y80KPC6w@cluster0.1nx30vc.mongodb.net/todolistDB";
+const client = new MongoClient(uri);
 
 app.set("view engine", "ejs");
 
@@ -16,12 +20,50 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.get("/", function (req, res) {
-    const day = date.getDate()
-    res.render("list", {
-        listTitle: day,
-        newListItems: items
-    });
+mongoose.connect(uri);
+
+const itemsSchema = {
+    name: String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your To Do List."
+});
+const item2 = new Item({
+    name: "Hit the '+' button to add a new item."
+});
+const item3 = new Item({
+    name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+async function seedDB() {
+    try {
+        await Item.insertMany(defaultItems);
+        console.log("Successfully added item to DB");
+    } catch (err) {
+        console.log(err);
+    } finally {
+        // Ensures that the client will close when you finish/error
+        client.close();
+    }
+};
+
+app.get("/", async function (req, res) {
+    try {
+        const foundItems = await Item.find({});
+        console.log(foundItems);
+        res.render("list", {
+            listTitle: "Today",
+            newListItems: foundItems
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error retrieving items from database");
+    }
 });
 
 app.get("/work", function (req, res) {
@@ -43,5 +85,9 @@ app.post("/", function (req, res) {
 });
 
 app.listen(3000, function () {
-    console.log("Sever 3000 up and running!");
+    seedDB().then(() => {
+        console.log("Server 3000 up and running!");
+    }).catch(err => {
+        console.log(err);
+    });
 });
